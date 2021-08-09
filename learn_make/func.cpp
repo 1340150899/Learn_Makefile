9a0d4022ccc9bcd60c67949d7c8f2c00dfafe20a
ubuntu <1340150899@qq.com>
2021-08-09 16:01:01 +0800

    makefile_1

---
+++ learn_make/func.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+
+void file_func(){
+    cout<< __FILE__<< endl;
+}
---
+++ learn_make/test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+void file_func();
+int main()
+{
+    file_func();
+    return 0;
+}
